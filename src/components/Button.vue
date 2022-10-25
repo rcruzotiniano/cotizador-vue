da0b80a4462ee8dfc375318610ceada9d64025dc
Richard Jorge Cruz Otiniano <rcruzotiniano@gmail.com>
2022-10-25 17:56:39 -0500

    Initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,12 @@
+<script setup>
+  const props = defineProps([
+      'operador',
+  ]);
+</script>
+
+<template>
+  <button
+      class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 text-white text-2zl"
+      @click="$emit('fn')"
+  >{{props.operador}}</button>
+</template>
